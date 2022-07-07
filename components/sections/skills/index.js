f5a312163f9795d6292140f73bf63eca44955c61
Elsa <elsahvy@gmail.com>
2022-07-07 14:15:49 -0700

    break up index grid into component sections

---
+++ components/sections/skills/index.js
@@ -0,0 +1,33 @@
+
+import {
+    Wrapper,
+    SkillsTitle
+} from './skills.styled'
+
+
+
+const skills = () => {
+  return (
+    <>
+<Wrapper>
+<SkillsTitle>Skills</SkillsTitle>
+                <ul>
+                <h3>Frontend Web Design</h3> 
+                          <li>Colorful websites for creative niches and small businesses.</li>
+                          <li>Quick and interactive styling</li>
+                          <li>Effective SEO</li>
+                          </ul> 
+                <ul>
+                <h3>Content Management</h3>  
+                          <li>Currently using Sanity.io for content writing, image management, and backend data storage</li>
+                          <li>I work one on one with customers to create the ideal website product</li>
+                          <li>In my freetime, I also work and manage my
+                             own projects
+                              </li> 
+                          </ul>
+                          </Wrapper>
+    </>
+  )
+}
+
+export default skills
--- components/styles/IndexGrid.styled.js
+++ components/styles/IndexGrid.styled.js
@@ -42,16 +42,7 @@ border-right: solid 2px black;
     @media only screen and (max-width: 768px) {
     }
 ` 
-
-export const JtImg = styled.div`
-padding-top: 500px;
-@media only screen and (max-width: 1024px) {
-  width: 100%;
-  height: 200px;
-  padding: 0;
-}
-`
-
+ 
 
 
 //*RIGHT
@@ -70,145 +61,7 @@ export const HomeRight = styled.div`
 }
 `
 
-
-
-//*INTRO SECTION
-export const IntroSection = styled.div`
- display: flex;
-justify-content: center;
-  flex-direction: column;
-padding: 75px 0 0;
-background: #fff;
-  @media only screen and (max-width: 1024px) {
-  margin: auto;
-  padding: auto;  
-  }
-`
-
-
-export const Intro = styled.h2`
-  color: #007ba5;
-  font-size: 35px;
-  text-align: left;
-  padding-left: 85px;
-@media only screen and (max-width: 768px) {
-  font-size: 40px;
-  margin-top: 25px;
-  line-height: 1.5em;
-  margin-bottom: 20px;
-}
-@media only screen and (max-width: 500px) {
-  padding: 0 20px;
-  margin: 0;
-}
-`
-
-export const Headline = styled.span`
-       text-align: center;
-       cursor: pointer;
-       font-size: 25px;
-       margin: 5px 30px;
-@media only screen and (max-width: 1024px) {
-      text-align: center;
-      cursor: pointer;
-      font-size: 27px;
-      line-height: 1.5em;
-    }
-    @media only screen and (max-width: 500px) {
-      padding: 20px;
-      margin: 0;
-    }
-    `
-
-//*ABOUT SECTION
-
-// export const PortfolioImg = styled.div`
-// height: 206px;
-// width: 500px;
-// `
-
-export const About = styled.div`
-
-padding: 55px;
-height: 500px;
-display: flex;
-h2 {
-  // color: #007ba5;
-}
-.portfolio-img {
-  border: solid 2px black;
-  width: 250px;
-  height: 400px;
-}
-@media only screen and (max-width: 500px) {
-  height: 1000px;
-display: flex;
-flex-direction: column;
-.portfolio-img {
-  border: solid 2px black;
-  margin: 0;
-  padding: 0;
-}
-}
-  `
-  
-
-export const Content = styled.div`
-h2 {
-  text-align: left;
-  margin: 10px 20px;
-  border-bottom: solid 2px teal;  
-}
-p {
-  margin: 15px;
-font-size: 20px;
-}
-@media only screen and (max-width: 500px) {
- p {
-  margin: 0;
-  padding: 0;
- }
-}
-`
-
-
-//*SKILLS SECTION
-
-export const SkillsTitle = styled.h2`
-text-align: center;
-font-size: 25px;
-text-decoration: underline;
-@media only screen and (max-width: 500px) {
-}
-`
-
-
-
-export const Skills = styled.div`
-border-top: solid 2px black;
-border-bottom: solid 2px black;
-padding: 35px;
-h3 {
-text-align: left;
-font-size: 25px;
-}
-ul {
-   margin: 25px;
-text-align: left;
-padding-left: 25px;
-
-}
-li {
-  font-size: 20px;
-  margin: 5px;
-  color: #007ba5;
-  font-weight: bold;
-  margin: 10px 0;
-}
-@media only screen and (max-width: 500px) {
-} 
-`
-
+ 
 
 
 
@@ -265,20 +118,4 @@ h2 {
 }
 }  
 `
-
  
-//*CONTACT SECTION
-export const Contact = styled.div`
-// background: #3aa1aa;
-border-top: solid 2px black;
-border-bottom: solid 2px black;
-text-align: center;
-font-weight: bold;
-height: 900px;
-h2 {
-  padding: 10px;
-}
-`
-
-
-
